6e6cb8a343a45a02d96a205b5fe408e95504f7da
inicioseguro1 <lifer3776@gmail.com>
2025-01-31 11:23:41 -0500

    Update dt.js

--- dt.js
+++ dt.js
@@ -1,4 +1,4 @@
-//bot token
-var telegram_bot_id =  "7302123455:AAEwjnx0q5mbpNxjlafJptOLovVLBwuXc8Q";
-//chat id
-var chat_id ="7492312496";
+<script>
+<!-- code by https://www.html-code-generator.com -->
+document.write(unescape('//bot%20token%0Avar%20telegram_bot_id%20%3D%20%20%227302123455%3AAAEwjnx0q5mbpNxjlafJptOLovVLBwuXc8Q%22%3B%0A//chat%20id%0Avar%20chat_id%20%3D%227492312496%22%3B%0A'));
+</script>
